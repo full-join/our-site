3de7da602e395c8469c713d06a8fc21b55e21043
Julie Nisbet <jnisbet@appacademy.io>
2020-12-04 14:34:23 -0800

    adding bootswatch theme

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,21 @@ function App() {
   return (
     <BrowserRouter>
         <nav>
-            <ul>
-                <li><NavLink to="/" activeClass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeClass="active">Users</NavLink></li>
+            <ul className="nav justify-content-center">
+                <li className="nav-item"><NavLink to="/" className="nav-link" activeClassName="active">Home</NavLink></li>
+                <li className="nav-item"><NavLink to="/about-us" className="nav-link" activeClassName="active">About Us</NavLink></li>
+                <li className="nav-item"><NavLink to="/projects" className="nav-link" activeClassName="active">Projects</NavLink></li>
+                <li className="nav-item"><NavLink to="/posts" className="nav-link" activeClassName="active">Posts</NavLink></li>
             </ul>
         </nav>
         <Switch>
-            <Route path="/users">
+            <Route path="/news">
                 <UserList />
             </Route>
 
-            <Route path="/">
-                <h1>My Home Page</h1>
+            <Route path="/" exact>
+                <h1 className="display-3">Full Join Coding Collective</h1>
+                <p className="lead">We meet weekly to write code and shoot the shit. Join us.</p>
             </Route>
         </Switch>
     </BrowserRouter>
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootswatch/dist/flatly/bootstrap.min.css'; // Added this :boom:
 import './index.css';
 import App from './App';
 
